7c50dba1fd1d07d22d52b2fdebace079789b7a55
Abhinav Rajaseshan <abhinavseshan@hotmail.com>
2016-12-01 17:13:18 -0500

    added lab4

---
+++ submit/Lab3/symbol.h
@@ -0,0 +1,21 @@
+#ifndef _SYMBOL_H
+#define _SYMBOL_H
+
+
+typedef struct _symbol_table_entry {
+	char *id;
+	int is_const;
+	int type_code;
+	int vec;
+	int is_init; //VERY IMPORTANT, the implementation for this is not straight forward
+	struct _symbol_table_entry *next;
+	struct _symbol_table_entry *sib;
+} symbol_table_entry;
+
+void scope_enter();
+void scope_exit();
+int symbol_exists_in_this_scope(char *id);
+void symbol_add(symbol_table_entry entry);
+symbol_table_entry* symbol_find(char *id);
+
+#endif
